0f4b702724fed0ce478283530bd755dbeca006f5
rizaldyzxz <127272778+rizaldyzxz@users.noreply.github.com>
2024-10-07 19:57:13 +0700

    Add files via upload

---
+++ 2311104051_Rizaldy Aulia Rachman/Unguided/Unguided.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Soal Nomor 1: Memisahkan Angka Genap dan Ganjil
+    // int n;
+
+    // cout << "Masukkan jumlah elemen array: ";
+    // cin >> n;
+
+    // int arr[n];
+
+    // cout << "Masukkan elemen array: ";
+    // for (int i = 0; i < n; ++i) {
+    //     cin >> arr[i];
+    // }
+
+    // cout << "Data Array: ";
+    // for (int i = 0; i < n; ++i) {
+    //     cout << arr[i] << " ";
+    // }
+
+    // cout << "\nNomor Genap: ";
+    // for (int i = 0; i < n; ++i) {
+    //     if (arr[i] % 2 == 0) {
+    //         cout << arr[i] << " ";
+    //     }
+    // }
+
+    // cout << "\nNomor Ganjil: ";
+    // for (int i = 0; i < n; ++i) {
+    //     if (arr[i] % 2 != 0) {
+    //         cout << arr[i] << " ";
+    //     }
+    // }
+
+    // return 0;
+
+
+    // Soal Nomor 2: Array Tiga Dimensi
+    // int main() {
+    // int dim1, dim2, dim3;
+
+    // cout << "Masukkan ukuran dimensi 1: ";
+    // cin >> dim1;
+    // cout << "Masukkan ukuran dimensi 2: ";
+    // cin >> dim2;
+    // cout << "Masukkan ukuran dimensi 3: ";
+    // cin >> dim3;
+
+    // int arr[dim1][dim2][dim3];
+
+    // // Input nilai untuk setiap elemen array (sesuaikan dengan kebutuhan)
+    // for (int i = 0; i < dim1; ++i) {
+    //     for (int j = 0; j < dim2; ++j) {
+    //         for (int k = 0; k < dim3; ++k) {
+    //             cout << "Masukkan nilai untuk arr[" << i << "][" << j << "][" << k << "]: ";
+    //             cin >> arr[i][j][k];
+    //         }
+    //     }
+    // }
+
+    // // Output nilai array (sesuaikan dengan format yang diinginkan)
+    // for (int i = 0; i < dim1; ++i) {
+    //     for (int j = 0; j < dim2; ++j) {
+    //         for (int k = 0; k < dim3; ++k) {
+    //             cout << arr[i][j][k] << " ";
+    //         }
+    //         cout << endl;
+    //     }
+    //     cout << endl;
+    // }
+
+    // return 0;
+
+
+    //Soal Nomor 3: Mencari Nilai Maksimum, Minimum, dan Rata-rata
+    
+//  int main() {
+//     int n;
+
+//     cout << "Masukkan jumlah elemen array: ";
+//     cin >> n;
+
+//     int arr[n];
+
+//     cout << "Masukkan elemen array: ";
+//     for (int i = 0; i < n; ++i) {
+//         cin >> arr[i];
+//     }
+
+//     int   
+//  max = arr[0], min = arr[0], sum = 0;
+//     for (int i = 1; i < n; ++i) {
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+//         if (arr[i] < min) {
+//             min = arr[i];
+//         }
+//         sum += arr[i];
+//     }
+
+//     double rataRata = (double)sum / n;
+
+//     cout << "Nilai maksimum: " << max << endl;
+//     cout << "Nilai minimum: " << min << endl;
+//     cout << "Nilai rata-rata: " << rataRata << endl;
+
+//     return 0;
+}
